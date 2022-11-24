4bb3f9499f7f5e790a48dc13642b1ac1e4d43af0
liamparker6 <liamparker19@virginmedia.com>
2022-11-24 19:37:34 +0000

    Added map, working enemies and collectibles

--- bullets.js
+++ bullets.js
@@ -5,17 +5,50 @@ class Ninjastar{
         this.y = y
         this.isRight = isRight
     }
-    spawnStar(scene){
+    spawnStar(scene, layer){
         this.ninjastar = scene.physics.add.sprite(this.x,this.y,'Star').setScale(1/4)
+        scene.physics.add.collider(this.ninjastar, layer, hitWall, null, scene)
+
+        this.ninjastar.setGravity(0,-1400)
+        for (let i = 0; i < enemies.length; i++)
+        {
+            scene.physics.add.overlap(this.ninjastar, enemies[i].grunt, hitEnemy, null, scene);
+        }
     }
     bulletMovement(){
         if(this.isRight){
             this.ninjastar.setVelocityX(-480)
-            this.ninjastar.setVelocityY(-25)
         }
         else{
             this.ninjastar.setVelocityX(480)
-            this.ninjastar.setVelocityY(-25)
         }
+        this.ninjastar.flipX = !this.ninjastar.flipX;
+    }
+}
+
+class EnemyShot{
+    enemyShot;
+    constructor(x,y, isRight){
+        this.x = x
+        this.y = y
+        this.isRight = isRight
+    }
+    makeSprite(scene, layer){
+        this.enemyShot = scene.physics.add.sprite(this.x,this.y,'Star').setScale(1/4)
+        scene.physics.add.collider(this.enemyShot, layer, hitWall, null, scene)
+        this.enemyShot.setGravity(0,-1400)
+        for (let i = 0; i < enemies.length; i++)
+        {
+            scene.physics.add.overlap(this.enemyShot, main.player, hitPlayer, null, scene);
+        }
+    }
+    shotMovement(){
+        if(this.isRight){
+            this.enemyShot.setVelocityX(-480)
+        }
+        else{
+            this.enemyShot.setVelocityX(480)
+        }
+        this.enemyShot.flipX = !this.enemyShot.flipX;
     }
 }
--- characters.js
+++ characters.js
@@ -12,7 +12,7 @@ class Player{
     {
         this.player = scene.physics.add.sprite(main.positionX, main.positionY,'Character').setScale(2);
     }
-    characterMovement(scene){
+    characterMovement(scene, layer){
         
         //Instructions for character movements 
         if(keyW.isDown && this.player.body.onFloor()){
@@ -31,7 +31,7 @@ class Player{
         }
         if(Phaser.Input.Keyboard.JustDown(keyE)){
             ninja.push(new Ninjastar(this.player.x, this.player.y, main.player.flipX));
-            ninja[ninja.length-1].spawnStar(scene)
+            ninja[ninja.length-1].spawnStar(scene, layer)
         }
     }
 }
@@ -42,44 +42,72 @@ class Player{
 
 class Grunt{
     grunt;
-    constructor(health, damage, positionX, positionY,isAlive){
+    constructor(health, damage, positionX, positionY, isAlive){
         this.health = health
         this.damage = damage
         this.positionX = positionX
         this.positionY = positionY
         this.isAlive = isAlive
         this.tickUpdate = 0
+        this.shootTimer = 0
         this.isMovingLeft = false
+        this.speed = Math.floor(Math.random() * 60) +20;
     }
-    makeSprite(scene)
+    makeSprite(scene, layer)
     {
-        this.grunt = scene.physics.add.sprite(enemy.positionX, enemy.positionY,'Grunt').setScale(2);
-    }
-    movement(){
+        this.grunt = scene.physics.add.sprite(this.positionX, this.positionY,'Grunt').setScale(2);
+        scene.physics.add.collider(this.grunt, layer);
+        this.grunt.x = this.positionX;
+        this.grunt.setAlpha(0.4)
+        for (let i = 0; i < ninja.length; i++)
+        {
+            scene.physics.add.overlap(this.grunt, ninja[i].ninjastar, hitEnemy, null, scene);
+        }
+    }   
+    movement(scene, layer){
         this.tickUpdate++
-
-        if(this.isAlive == true && this.tickUpdate > 60 && this.isMovingLeft == true){
-            this.moveleft()
+        if(this.isAlive == true && this.tickUpdate > this.speed && this.isMovingLeft == true){
+            this.moveleft(scene, layer)
             this.isMovingLeft = false
             this.tickUpdate = 0
 
         }
 
         
-        if(this.isAlive == true && this.tickUpdate > 60 && this.isMovingLeft == false){
-            this.moveright()
+        if(this.isAlive == true && this.tickUpdate > this.speed && this.isMovingLeft == false){
+            this.moveright(scene, layer)
             this.isMovingLeft = true
             this.tickUpdate = 0
 
         }
     }
-    moveleft(){
+    moveleft(scene, layer){
         this.grunt.setVelocityX(-160);
         this.grunt.flipX = true;
+        this.isShooting(scene, layer)
     }
-    moveright(){
+    moveright(scene, layer){
         this.grunt.setVelocityX(160);
         this.grunt.flipX = false;
+        this.isShooting(scene, layer)
+    }
+
+    isShooting(scene, layer){
+        this.shootTimer++
+        if(this.isAlive == true && this.shootTimer > 1){
+            gruntBullets.push(new EnemyShot(enemies[i].grunt.x, enemies[i].grunt.y, enemies[i].grunt.flipX))
+            gruntBullets[gruntBullets.length-1].makeSprite(scene, layer)
+            this.shootTimer = 0 
+        }
+    }
+
+    destroyMeNow(){
+        this.grunt.destroy();
+    }
+
+    stopAlive(){
+        //this.isAlive = false;
+        //console.log(grunt.isAlive + "XXXX" + this.isAlive)
     }
 }
 
